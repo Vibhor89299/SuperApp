f507fb943cd14a824f8cf395691724443fdac476
VibhorR <vibhorrawat95@gmail.com>
2023-05-25 15:15:52 +0530

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import { HashRouter , Route , Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Registration from "./Pages/Registration/Registration";
 import Category from "./Pages/Category/Category";
 
-
 function App() {
   return (
-    <>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-         <Route path='/' element={<Registration /> } />
-         <Route path='/category' element={<Category />} />
+        <Route path="/" element={<Registration />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
     </HashRouter>
-    </>
   );
 }
 
