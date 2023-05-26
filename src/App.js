60a5b7901532780396826f16f7925cf715b80c4c
VibhorR <vibhorrawat95@gmail.com>
2023-05-26 20:17:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "./Pages/Registration/Registration";
 import Category from "./Pages/Category/Category";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="superapp/" element={<Registration />} />
         <Route path="superapp/category" element={<Category />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
