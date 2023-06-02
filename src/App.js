aa7f6ed8de19dc1c9db578c1e4e3be1e0e6b8101
VibhorR <vibhorrawat95@gmail.com>
2023-06-02 10:42:51 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Registration from "./Pages/Registration/Registration";
 import Category from "./Pages/Category/Category";
+import Home from './Pages/Home/Home';
+import BrowseEntertainment from './Pages/BrowseEntertainment/BrowseEntertainment';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="/category" element={<Category />} />
+        <Route path='/' element={<Registration />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/browse' element={<BrowseEntertainment />} />
       </Routes>
     </HashRouter>
   );
